84199ba390c204ff133394d03386751a0013d976
alyssatiarra <tiarraalyssa@gmail.com>
2024-03-19 12:05:35 +0700

    Pertemuan 5 - Praktikum 1

---
+++ Pertemuan5/array1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head></head>
+    <body>
+        <h2>Array Terindeks</h2>
+        <?php
+            // $Listdosen=["Elok Nur Hamdana", "Unggul //Pamenang", "Bagas Nugraha"];
+            // foreach ($Listdosen as $dosen)
+            // echo $dosen, "<br>";
+            // // echo $Listdosen[2] . "<br>";
+            // // echo $Listdosen[0] . "<br>";
+            // // echo $Listdosen[1] . "<br>";
+            $Listdosen=["Elok Nur Hamdana", "Unggul Pamenang", "Bagas Nugraha"];
+            for ($i = 0; $i < 4; $i++){
+                echo $Listdosen[$i] . "<br>";
+            }
+        ?>
+    </body>
+</html>
