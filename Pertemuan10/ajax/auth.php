7c0edb69f2fa138f604c9ccafa5a569b7171a5b7
alyssatiarra <tiarraalyssa@gmail.com>
2024-04-14 19:11:01 +0700

    Pertemuan 10 - Praktikum 6

---
+++ Pertemuan10/ajax/auth.php
@@ -0,0 +1,7 @@
+<?php
+    // buat token keamanan
+    session_start();
+    if (empty($_SESSION['csrf_token'])){
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+?>
---
+++ Pertemuan10/ajax/crsf.php
@@ -0,0 +1,16 @@
+<?php
+    header('Content-Type: application/json');
+
+    if(empty($_SESSION['csrf_token'])){
+        $_SESSION['csrf_token']=bin2hex(random_bytes(32));
+    }
+
+    $header = apache_request_headers();
+    if(isset($header['Csrf-Token'])){
+        if($header['Csrf-Token'] !== $_SESSION['csrf_token']){
+            exit(json_encode(['error' => 'Wrong CSRF token.']));
+        }
+    } else {
+        exit (json_encode(['error' => 'No CSRF token.']));
+    }
+?>
